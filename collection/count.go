9e691f9c1cefabc6baf6cdbb81f36addc727e7a5
Chris Miles <chris.miles.e@gmail.com>
2025-12-07 18:38:05 -0600

    moving

--- collection/count.go
+++
@@ -1,9 +0,0 @@
-package collection
-
-// Count returns the total number of items in the collection.
-// Example:
-//   c := collection.New([]int{1, 2, 3, 4})
-//   count := c.Count() // 4
-func (c Collection[T]) Count() int {
-	return len(c.items)
-}
