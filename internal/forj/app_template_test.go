73c4adeaa0fed5b948201466dcc1bae852ae29ef
Chris Miles <chris.miles.e@gmail.com>
2026-04-05 16:55:57 -0500

    chore(app): debug log lifecycle

--- internal/forj/app_template_test.go
+++ internal/forj/app_template_test.go
@@ -35,7 +35,7 @@ func TestWireAppTemplateUsesSingularDefaultAndPluralManagers(t *testing.T) {
 		"defaultQueue.StartWorkers",
 		`appTimeouts.QueueShutdownTimeout()`,
 		`app.NewLifecycle(appTimeouts)`,
-		`logger.Info().Msg("Shutting down database connections...")`,
+		`logger.Debug().Msg("Shutting down database connections...")`,
 		`func (a *App) appShutdownTimeout() time.Duration`,
 	} {
 		if !strings.Contains(source, snippet) {
